b15e37e78626e1e48532dacdccdf8229c21bf75f
equalshector <veiga.hector@gmail.com>
2016-12-10 11:46:47 -0800

    finished tutorial

--- src/js/client.js
+++ src/js/client.js
@@ -1,6 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import Layout from "./components/Layout"
+import { Router, Route, IndexRoute, hashHistory } from "react-router"
+
+import Layout from "./components/layout/Default"
 
 const app = document.getElementById('app')
-ReactDOM.render(<Layout/>, app)
+
+ReactDOM.render(
+    <Router history={hashHistory}>
+        <Route path="/" component={Layout}>
+        </Route>
+    </Router>,
+
+app)
